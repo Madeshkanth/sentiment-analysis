25fa0dc217547d3b95408b71edf5641970173173
Madeshkanth S <143262733+Madeshkanth@users.noreply.github.com>
2024-11-25 22:47:25 +0530

    Update sentiment analysis.py

--- sentiment analysis.py
+++ sentiment analysis.py
@@ -110,5 +110,5 @@ from transformers import pipeline
 
 sent_pipeline = pipeline("sentiment-analysis")
 sent_pipeline('I love sentiment analysis!')
-sent_pipeline('Make sure to like and subscribe!')
+sent_pipeline('Good product')
 sent_pipeline('booo')
